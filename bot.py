f6550001cb5183a26398edd9b9d4ae5a7e6ec190
Brandon Wong <bwong19@jhu.edu>
2019-12-03 02:11:53 -0500

    init

---
+++ bot.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException  
+from selenium.webdriver.support.ui import Select 
+import datetime
+import os 
+import sys
+
+EMAILFIELD = (By.ID, "i0116")
+PASSWORDFIELD = (By.ID, "i0118")
+NEXTBUTTON = (By.ID, "idSIButton9")
+
+try:
+	usernameStr = sys.argv[1]
+	passwordStr = sys.argv[2]
+except BaseException:
+	print("\nError: This script should be run with the following (valid) flags:\n python bot.py SIS_Username SIS_Password\n")
+	sys.exit(-1)
+
+browser = webdriver.Chrome()
+browser.get(('https://sis.jhu.edu/sswf/'))
+
+nextButton = browser.find_element_by_id('linkSignIn')
+nextButton.click()
+
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable(EMAILFIELD)).send_keys(usernameStr)
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable(NEXTBUTTON)).click()
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable(PASSWORDFIELD)).send_keys(passwordStr)
+WebDriverWait(browser, 10).until(EC.element_to_be_clickable(NEXTBUTTON)).click()
+
+
+WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "aspnetForm")))
+browser.get("https://sis.jhu.edu/sswf/SSS/EnrollmentCart/SSS_EnrollmentCart.aspx?MyIndex=88199")
+
+
+WebDriverWait(browser, 10)
+selectAll = browser.find_element_by_id('SelectAllCheckBox')
+selectAll.click()
+
+WebDriverWait(browser, 10)
+register = browser.find_element_by_id("ctl00_contentPlaceHolder_ibEnroll")
+
+# # Wait until its 7 O'clock
+while True:
+	current_hour = datetime.datetime.now().time().hour
+	if current_hour >= 7:
+		browser.execute_script("arguments[0].click();", register)
+		WebDriverWait(browser, 10000)
+		break
+
